139b200caa1b17cb379b6c8413a91abcb0dab57e
olasosz <43154874+olasosz@users.noreply.github.com>
2019-01-17 11:53:28 -0500

    nptest

---
+++ npTest/sketch.js
@@ -0,0 +1,22 @@
+var = 0;
+function setup() 
+{
+
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  text(r,width/2,height/2);
+ 
+}
+
+
+///uses built in deviceShaken function in p5
+function deviceShaken() 
+{
+	r = r+20;
+
+}
+
+
