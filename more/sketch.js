6028e476fe20d4c9e8e08bfe075c5e7880afafdc
olasosz <43154874+olasosz@users.noreply.github.com>
2019-01-22 09:19:19 -0500

    began separate coal code

---
+++ more/sketch.js
@@ -0,0 +1,64 @@
+
+
+var dataServer;
+var pubKey = 'pub-c-8efd8f87-9fe2-45a5-81f6-7b60513f5ddc';
+var subKey = 'sub-c-43574d8c-135b-11e9-abd1-2a488504b737';
+
+//input variables
+var coal = 100;
+
+
+//name used to sort your messages. used like a radio station. can be called anything
+var channelName = "train";
+
+function setup() 
+{
+  button = createButton('coal');
+  button.size(100,100);
+  button.position(65, 65);
+  button.mousePressed(moreCoal);
+
+  getAudioContext().resume();
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  
+ 
+   // initialize pubnub
+  dataServer = new PubNub(
+  {
+    publish_key   : pubKey,  //get these from the pubnub account online
+    subscribe_key : subKey,  
+    ssl: true  //enables a secure connection. This option has to be used if using the OCAD webspace
+  });
+
+    noStroke();
+    fill(0);  
+    //textSize(90);
+    //text("!SHOVEL!", width/2, height/2);
+
+
+}
+
+function draw() {
+background(0);
+}
+    console.log(coal);
+}
+
+
+//console.log(slideNumber);
+
+  //publish the number to everyone.
+  dataServer.publish(
+    {
+      channel: channelName,
+      message: 
+      {
+        slide: slideNumber       
+      }
+    });
+
+
+function moreCoal() {
+  coal+=20;
+}
